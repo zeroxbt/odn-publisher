afd2454542a096a7bbac32ae5f0e8c0a109f8631
zeroxbt <original.swazi@protonmail.com>
2022-01-09 22:36:26 +0100

    added sleep between publish calls

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const getRandomGoogle = require("./src/queryGoogleKG");
 const publishToODN = require("./src/queryOTNode");
 require("dotenv").config();
 
+const sleepForMilliseconds = async (milliseconds) => {
+  await new Promise((r) => setTimeout(r, milliseconds));
+};
+
 const googleDataset = "google.json";
 const publish = async () => {
   getRandomGoogle()
     .then(async ({ assets, keywords }) => {
       await publishToODN(assets, keywords, googleDataset);
+      await sleepForMilliseconds(6 * 1000);
       publish();
     })
     .catch((err) => console.log(`Error : ${error}`));
