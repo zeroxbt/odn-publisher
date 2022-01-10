cc43ff82da07c7a96d22babbcb222a8249b7353f
zeroxbt <original.swazi@protonmail.com>
2022-01-10 11:47:22 +0100

    .

--- src/queryGoogleKG.js
+++ src/queryGoogleKG.js
@@ -36,10 +36,7 @@ module.exports = getRandomGoogle = async () => {
     "@type": query,
   };
 
-  await fs.promises.writeFile(
-    `./../datasets/wikidata.json`,
-    JSON.stringify(dataSet)
-  );
+  await fs.promises.writeFile(`/datasets/google.json`, JSON.stringify(dataSet));
 
   console.log("Finished writing dataset!");
 
