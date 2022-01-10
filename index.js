ef4b993bd54888b65e741c1cf4ada1017f679de7
zeroxbt <original.swazi@protonmail.com>
2022-01-10 15:51:58 +0100

    fix to queryType import

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const randomQuery = require("./src/util/queryTypes");
 require("dotenv").config();
 
 const publish = async () => {
-  console.log(`About to publish dataset taken from ${randomQuery.name}`);
-  randomQuery
+  const query = randomQuery();
+  console.log(`About to publish dataset taken from ${query.name}`);
+  query()
     .getData()
     .then(async ({ assets, keywords }) => {
-      await publishToODN(assets, keywords, randomQuery.filepath);
+      await publishToODN(assets, keywords, query.filepath);
       publish();
     })
     .catch((error) => console.log(`Error : ${error}`));
