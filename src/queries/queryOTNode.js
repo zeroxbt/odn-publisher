e88537ecf94d9e4aac1ad9580fb65fef49cddf58
root <tybraha@gmail.com>
2022-01-27 23:56:26 +0000

    Added configuration to turn on self search of the most recently published dataset

--- src/queries/queryOTNode.js
+++ src/queries/queryOTNode.js
@@ -11,19 +11,70 @@ let options = {
 };
 const dkg = new DKGClient(options);
 
-module.exports = publish = async (assets, keywords, filepath) => {
-  options = {
-    filepath,
-    assets,
-    keywords,
-    visibility: true,
-  };
-  await dkg
-    .publish(options)
-    .then((result) => {
-      console.log(JSON.stringify(result));
-    })
-    .catch((error) => {
-      console.log(`error publishing to the network. ${error}`);
-    });
+// module.exports = publish = async (assets, keywords, filepath) => {
+//   options = {
+//     filepath,
+//     assets,
+//     keywords,
+//     visibility: true,
+//   };
+//   await dkg
+//     .publish(options)
+//     .then((result) => {
+//       console.log(JSON.stringify(result));
+//     })
+//     .catch((error) => {
+//       console.log(`error publishing to the network. ${error}`);
+//     });
+// };
+
+module.exports = {
+  publish: async function publish(assets, keywords, filepath) {
+    options = {
+      filepath,
+      assets,
+      keywords,
+      visibility: true,
+    };
+    await dkg
+      .publish(options)
+      .then((result) => {
+        console.log(JSON.stringify(result));
+      })
+      .catch((error) => {
+        console.log(`error publishing to the network. ${error}`);
+      });
+  },
+  search: async function search(keywords) {
+    type = Math.floor(Math.random() * 2);
+
+    if(type == '0'){
+      type = 'entities'
+    }
+
+    if(type == '1'){
+      type = 'assertions'
+    }
+
+    console.log(`Searching for type: ${type}`);
+    console.log(`Searching for keywords: ${keywords}`);
+
+    options = {
+      query: keywords,
+      resultType: type
+     };
+
+     await dkg
+     .search(options)
+     .then((result) => {
+ 			if(result.status == 'FAILED'){
+        console.log('\x1b[31mSearch Failed!')
+        console.log('\x1b[31m',result.error)
+ 			}
+   			console.log('\x1b[32mSearch Completed!')
+   			console.log(' ')
+   			console.log('\x1b[32mHeres a string of the result data!')
+        console.log('\x1b[32m',JSON.stringify(result.data));
+ 			});
+  },
 };
