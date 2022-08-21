788e26c707faa1bb8a45d331495c9757171fff1a
zeroxbt <original.swazi@protonmail.com>
2022-08-21 21:38:09 +0200

    add log result

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const { setTimeout } = require("timers/promises");
 const { publish, get } = require("./src/apis/queryOTNode");
 const apis = require("./src/util/apis");
 
+const logPrettyJSON = (data) => console.log(JSON.stringify(data, null, 2));
+
 (async () => {
   while (true) {
     for (const api of apis) {
       console.log(`About to publish dataset taken from ${api.name}`);
+
+      // fetch data
       const data = await api.getData();
+
+      // create asset
       const publishResult = await publish(data);
+      logPrettyJSON(publishResult)
+
+      // get asset
       if (publishResult?.UAL) {
-        get(publishResult.UAL);
+        const getResult = await get(publishResult.UAL);
+        logPrettyJSON(getResult)
       }
+
+      // sleep 5 seconds
       await setTimeout(5 * 1000);
     }
   }
